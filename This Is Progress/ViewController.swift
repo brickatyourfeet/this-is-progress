780350ebea4893247059fdfbf981b2146ecfc23d
brickatyourfeet <brickatyourfeet@gmail.com>
2020-01-01 15:26:14 -0800

    Initial Commit

---
+++ This Is Progress/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  This Is Progress
+//
+//  Created by Kyle Braden on 1/1/20.
+//  Copyright © 2020 Kyle Braden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
